256dece659e4d856b7622d8dbdd6f4ac0846eb78
Fernandox64 <fernandox64@gmail.com>
2015-07-09 02:37:43 -0300

    Setup paperclip funcionando

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ActiveRecord::Base
 	belongs_to :user
+	has_attached_file :image, styles: { medium: "700x500#", small: "350x250>" }, :default_url => "/assets/images/feop_logo.jpg"
+    validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150709043154) do
+ActiveRecord::Schema.define(version: 20150709051316) do
 
   create_table "editals", force: :cascade do |t|
     t.string   "name",             limit: 255
@@ -26,12 +26,16 @@ ActiveRecord::Schema.define(version: 20150709043154) do
   end
 
   create_table "posts", force: :cascade do |t|
-    t.string   "title",       limit: 255
-    t.string   "link",        limit: 255
-    t.text     "description", limit: 65535
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
-    t.integer  "user_id",     limit: 4
+    t.string   "title",              limit: 255
+    t.string   "link",               limit: 255
+    t.text     "description",        limit: 65535
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.integer  "user_id",            limit: 4
+    t.string   "image_file_name",    limit: 255
+    t.string   "image_content_type", limit: 255
+    t.integer  "image_file_size",    limit: 4
+    t.datetime "image_updated_at"
   end
 
   create_table "products", force: :cascade do |t|
