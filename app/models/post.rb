f286cdd69fa559df9d798e8e16f4c03d0d39ae14
Fernandox64 <fernandox64@gmail.com>
2015-07-29 08:05:41 -0300

    menu de navegação

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
 	acts_as_votable
 	belongs_to :user
 	has_many :comments
-	has_attached_file :image, styles: { medium: "700x500#", small: "350x250>" }, :default_url => "/assets/images/feop_logo.jpg"
+	has_attached_file :image, styles: { medium: "700x500#", small: "350x250>" }, :default_url => "/assets/images/feop_logo.jpg",
     validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
 end
